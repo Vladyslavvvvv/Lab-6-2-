ec243e324bde3a500bb58e89a2d5101ef872aa09
Vlad <matviykiv.v@nltu.lviv.ua>
2023-12-11 01:24:34 +0200

    ОООО

--- Lab 6(2).cpp
+++ Lab 6(2).cpp
@@ -113,71 +113,35 @@ int main() {
     cout << "Enter the number of rows and columns: ";
     cin >> rows >> columns;
 
-    MatrixCopy<int> matrixCopy(rows, columns);
-
+    MatrixCopy<int> intMatrix(rows, columns);
     int minValue, maxValue;
-    cout << "Enter the minimum and maximum values for random filling: ";
+    cout << "Enter the minimum and maximum values for random filling (int): ";
     cin >> minValue >> maxValue;
+    intMatrix.fillRandomValues(minValue, maxValue);
 
-    matrixCopy.fillRandomValues(minValue, maxValue);
-
-    cout << "Generated Matrix:\n";
-    matrixCopy.printMatrix();
+    cout << "Generated Integer Matrix:\n";
+    intMatrix.printMatrix();
     cout << "-----------------\n";
 
-    int choice;
-    cout << "Choose operation:\n";
-    cout << "1. Copy by rows\n";
-    cout << "2. Copy by columns\n";
-    cout << "3. Copy selected row\n";
-    cout << "4. Copy selected column\n";
-    cout << "5. Copy main diagonal\n";
-    cin >> choice;
+    MatrixCopy<double> doubleMatrix(rows, columns);
+    double doubleMinValue, doubleMaxValue;
+    cout << "Enter the minimum and maximum values for random filling (double): ";
+    cin >> doubleMinValue >> doubleMaxValue;
+    doubleMatrix.fillRandomValues(doubleMinValue, doubleMaxValue);
 
-    switch (choice) {
-    case 1: {
-        int rowToCopy;
-        cout << "Enter the row to copy: ";
-        cin >> rowToCopy;
-        matrixCopy.copyByRows(rowToCopy);
-        break;
-    }
-    case 2: {
-        int colToCopy;
-        cout << "Enter the column to copy: ";
-        cin >> colToCopy;
-        matrixCopy.copyByColumns(colToCopy);
-        break;
-    }
-    case 3: {
-        int sourceRow, destRow;
-        cout << "Enter the source and destination rows: ";
-        cin >> sourceRow >> destRow;
-        matrixCopy.copySelectedRow(sourceRow, destRow);
-        break;
-    }
-    case 4: {
-        int sourceCol, destCol;
-        cout << "Enter the source and destination columns: ";
-        cin >> sourceCol >> destCol;
-        matrixCopy.copySelectedColumn(sourceCol, destCol);
-        break;
-    }
-    case 5: {
-        matrixCopy.copyMainDiagonal();
-        cout << "Main Diagonal:\n";
-        matrixCopy.printMainDiagonal();
-        break;
-    }
-    default:
-        cout << "Invalid choice\n";
-        break;
-    }
+    cout << "Generated Double Matrix:\n";
+    doubleMatrix.printMatrix();
+    cout << "-----------------\n";
 
-    if (choice != 5) {
-        cout << "Result Matrix:\n";
-        matrixCopy.printMatrix();
-    }
+    MatrixCopy<char> charMatrix(rows, columns);
+    char charMinValue, charMaxValue;
+    cout << "Enter the minimum and maximum values for random filling (char): ";
+    cin >> charMinValue >> charMaxValue;
+    charMatrix.fillRandomValues(charMinValue, charMaxValue);
+
+    cout << "Generated Char Matrix:\n";
+    charMatrix.printMatrix();
+    cout << "-----------------\n";
 
     return 0;
 }
